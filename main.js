1161525a6a7eec1947ef9a3db0ea6ce63e0b880a
Adam <aalinauskas@gmail.com>
2016-11-26 16:15:19 -0700

    Delete delete creeps from memory

--- main.js
+++ main.js
@@ -3,10 +3,21 @@ var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
 
 module.exports.loop = function () {
+
+    for(var name in Memory.creeps) {
+            if(!Game.creeps[name]) {
+                delete Memory.creeps[name];
+                console.log('Clearing non-existing creep memory:', name);
+            }
+        }
+
+
     
     roleHarvester.create(Game); 
     roleUpgrader.create(Game);
     roleBuilder.create(Game);
+
+    
 
      for(var name in Game.creeps) {
         var creep = Game.creeps[name];
