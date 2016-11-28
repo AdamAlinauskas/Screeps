05fd9c2003f764cee8ed8cd76ac032bc81369a35
Adam <aalinauskas@gmail.com>
2016-11-27 17:11:13 -0700

    Only create builders if there are things to build

--- role.builder.js
+++ role.builder.js
@@ -27,9 +27,12 @@ var roleBuilder = {
     create: function(game){
         var creeps = _.filter(game.creeps,(creep)=>creep.memory.role === 'builder');
        //TODO find way to check for structures without using creeps
-	    if(creeps.length < 3){
-	        game.spawns.Spawn1.createCreep([MOVE,MOVE, CARRY, WORK],{role:'builder'})
-	    }
+	   //only create builders if there are things to build.
+	    var harvesters = _.filter(game.creeps,(creep)=>creep.memory.role === 'harvester');
+		if(harvesters.length >1 && harvesters[0].room.find(FIND_CONSTRUCTION_SITES).length > 1)
+			if(creeps.length < 3){
+				game.spawns.Spawn1.createCreep([MOVE,MOVE, CARRY, WORK],{role:'builder'})
+			}
     }
     
 };
