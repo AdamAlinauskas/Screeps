ad03bd3102bdcec8241dc99530b788d722bad3ef
Adam Alinauskas <aalinauskas@gmail.com>
2016-12-03 14:59:59 -0700

    Improving the harvesters

--- role.harvester.js
+++ role.harvester.js
@@ -5,7 +5,20 @@ var roleHarvester = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-	    if(creep.carry.energy < creep.carryCapacity) {
+        
+        
+        if(!creep.memory.harvesting && creep.carry.energy == 0) {
+            creep.memory.harvesting = true;
+            creep.say('harvesting');
+	    }
+	    
+	    if(creep.memory.harvesting && creep.carry.energy == creep.carryCapacity) {
+	        creep.memory.harvesting = false;
+	        creep.say('not harvesting')
+	        
+	    }
+        
+	    if(creep.memory.harvesting && creep.carry.energy < creep.carryCapacity) {
             var sources = creep.room.find(FIND_SOURCES);
 
 			//Find closest
@@ -14,37 +27,32 @@ var roleHarvester = {
             if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(source);
             }
+            return;
         }
 		//else if(Game.spawns['Spawn1'].energy < Game.spawns['Spawn1'].energyCapacity) {
-            else{
-                
-            var targets = creep.room.find(FIND_STRUCTURES, {
-                    filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
-                            structure.energy < structure.energyCapacity;
-                    }
-            });
-
+            
+            
+        var targets = creep.room.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN)
+                }
+        });
         
-
-            if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE ) {
-                creep.moveTo(targets[0]);
+        var targetsThatNeedEnergy = targets.filter((x)=>x.energy < x.energyCapacity);
+        
+        if(targetsThatNeedEnergy.length >0){
+            if(creep.transfer(targetsThatNeedEnergy[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE ) {
+                creep.moveTo(targetsThatNeedEnergy[0]);
             }
         }
 
-        var atMaxEnergy = creep.room.find(FIND_STRUCTURES, {
-                    filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN);
-                    }
-            }).every(function(structure){return structure.energy == structure.energyCapacity});
+        if(targets.every((structure) => structure.energy == structure.energyCapacity)){
 
-        
-        if(atMaxEnergy){
             //Spawn's enerty is maxed out put harvester to use and upgrade the conrollers
             creep.say('Swiching to upgrading')
             roleUpgrader.upgrade(creep);
         }
-      
+        
 	},
 		
 	create: function(game,potentialEnergyStored){
