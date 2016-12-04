dbc5f859f4a54cbd62675deb931e6cb805ec9551
Adam Alinauskas <aalinauskas@gmail.com>
2016-12-04 11:14:47 -0700

    creepHelper will create a worker creep that uses as much energy as possible.

--- creep.helper.js
+++ creep.helper.js
@@ -1,21 +1,31 @@
 var creepHelper = {
     CreateWorkerCreep: function(roleName, potentialEnergyStored){
-         //TOUGHT = 10 X 0 = 0
+        //TOUGHT = 10 X 0 = 0
         //Attack = 80 X 0 = 0
         //MOVE = 50 X 2 = 100
         //CARRY = 50 X 2 = 100
         //WORK = 100 X 2 = 200
         //TOTAL = 400
+
+        //move(50) carry(50) work(100) = 200
          
 	   var creeps = _.filter(Game.creeps,(creep)=>creep.memory.role === roleName);
 
 	    if(creeps.length < 4){
-            var parts = [MOVE, CARRY, WORK];
-            if(potentialEnergyStored >=400){
-                parts = [MOVE,MOVE,CARRY,CARRY,WORK,WORK];
+            
+            var partsToAppend = [MOVE,CARRY,WORK];
+            var selectedParts = [MOVE,CARRY,WORK];
+            var eneergyRequiredToCreateCreep = 200;
+            
+            while(eneergyRequiredToCreateCreep < potentialEnergyStored){
+                if(eneergyRequiredToCreateCreep + 200 > potentialEnergyStored)
+                    return;
+
+                selectedParts = selectedParts.concat(partsToAppend);
+                eneergyRequiredToCreateCreep = eneergyRequiredToCreateCreep + 200;
             }
 
-	        Game.spawns.Spawn1.createCreep(parts,{role:roleName})
+	        Game.spawns.Spawn1.createCreep(selectedParts,{role:roleName})
 	    }
 	
     }
